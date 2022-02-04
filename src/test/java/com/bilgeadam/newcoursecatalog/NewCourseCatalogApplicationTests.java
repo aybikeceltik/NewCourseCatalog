89e958e2c32f3f9da59fbc3edd22e84a0f0aa191
x <celtikkaybike@gmail.com>
2022-02-05 00:14:18 +0300

    Initial commit

---
+++ src/test/java/com/bilgeadam/newcoursecatalog/NewCourseCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.newcoursecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCourseCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
